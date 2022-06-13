262a54becb1c5aa379ea31b4029d72f634191e34
Scribble <scribble@minecrafttas.com>
2022-06-13 23:56:00 +0200

    Config, slash commands, logging...

--- src/main/java/com/minecrafttas/tascomp/TASCompBot.java
+++ src/main/java/com/minecrafttas/tascomp/TASCompBot.java
@@ -1,24 +1,52 @@
 package com.minecrafttas.tascomp;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.InvalidPropertiesFormatException;
 import java.util.Properties;
 
 import javax.security.auth.login.LoginException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.minecrafttas.tascomp.util.EmoteWrapper;
+import com.minecrafttas.tascomp.util.Util;
+import com.vdurmont.emoji.EmojiManager;
+
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.MessageReaction.ReactionEmote;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import net.dv8tion.jda.internal.interactions.CommandDataImpl;
 
 public class TASCompBot extends ListenerAdapter implements Runnable {
 
+	private static TASCompBot instance;
 	private final JDA jda;
 	private final Properties configuration;
-	private final HashMap<Long, Properties> guildConfigs = new HashMap<>();
+	private final Properties defaultCuildConfig = createDefaultGuildProperties();
+	private static final HashMap<Long, Properties> guildConfigs = new HashMap<>();
+	private static final File configDir = new File("configs/");
+	private static final Logger LOGGER = LoggerFactory.getLogger("TAS Competition");
 	
 	public TASCompBot(Properties configuration) throws InterruptedException, LoginException {
+		instance=this;
 		this.configuration = configuration;
 		final JDABuilder builder = JDABuilder.createDefault(this.configuration.getProperty("token"))
 				.setMemberCachePolicy(MemberCachePolicy.ALL)
@@ -31,10 +59,139 @@ public class TASCompBot extends ListenerAdapter implements Runnable {
 	@Override
 	public void run() {
 		/* Register the Commands */
-		System.out.println("[TAS Competition] Preparing Bot...");
-		for (Guild guild : jda.getGuilds()) {
-			
+		LOGGER.info("Preparing bot...");
+		if(!configDir.exists()) {
+			configDir.mkdir();
 		}
-		System.out.println("[TAS Competition] Done preparing bot.");
+		
+		for (Guild guild : jda.getGuilds()) {
+			prepareGuild(guild);
+		}
+		
+		LOGGER.info("Done preparing bot!");
+	}
+	
+	@Override
+	public void onGuildJoin(GuildJoinEvent event) {
+		LOGGER.info("Bot joined new guild {}.", event.getGuild().getName());
+		prepareGuild(event.getGuild());
+	}
+	
+	private void prepareGuild(Guild guild) {
+		LOGGER.info("Preparing guild {}...", guild.getName());
+		prepareCommands(guild);
+		Properties guildConfig = new Properties();
+		guildConfig.putAll(defaultCuildConfig);
+		
+		File configFile = new File(configDir, guild.getId()+".xml"); 
+		if(configFile.exists()) {
+			guildConfig = loadConfig(guild, configFile);
+		} else {
+			LOGGER.info("Creating default config...");
+			saveConfig(guild, guildConfig);
+		}
+		guildConfigs.put(guild.getIdLong(), guildConfig);
+		LOGGER.info("Done preparing guild {}!", guild.getName());
+	}
+
+	private void prepareCommands(Guild guild) {
+		LOGGER.info("Preparing commands...");
+		CommandListUpdateAction updater = guild.updateCommands();
+		
+		// =========================Set Channel
+		CommandDataImpl setChannelCommand = new CommandDataImpl("setchannel", "Set's the current channel to the bots channel");
+		setChannelCommand.setDefaultEnabled(false);
+		
+		OptionData typeOption = new OptionData(OptionType.STRING, "type", "The type of information channel. Can be ORGANIZER, SUBMIT", true);
+		
+		SubcommandData addSubCommand = new SubcommandData("add", "Adds this channel to the config");
+		addSubCommand.addOptions(typeOption);
+		
+		SubcommandData removeSubCommand = new SubcommandData("remove", "Removes this channel from the config");
+		removeSubCommand.addOptions(typeOption);
+		
+		SubcommandData listSubCommand = new SubcommandData("list", "Lists the current modifiers for this channel");
+		
+		setChannelCommand.addSubcommands(addSubCommand, removeSubCommand, listSubCommand);
+		
+		// =========================
+		
+		updater.addCommands(setChannelCommand);
+		updater.queue();
+		LOGGER.info("Done preparing commands!");
+	}
+	
+	public static Properties loadConfig(Guild guild, File configFile) {
+		LOGGER.info("Loading config for guild {}...", guild.getName());
+		Properties guildConfig=new Properties();
+		try {
+			FileInputStream fis = new FileInputStream(configFile);
+			guildConfig.loadFromXML(fis);
+			fis.close();
+		} catch (InvalidPropertiesFormatException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return guildConfig;
+	}
+	
+	public static void saveConfig(Guild guild, Properties config) {
+		LOGGER.info("Saving config for guild {}...", guild.getName());
+		File configFile = new File(configDir, guild.getId()+".xml"); 
+		try {
+			FileOutputStream fos=new FileOutputStream(configFile);
+			config.storeToXML(fos, "Properties for guild: " + guild.getName(), "UTF-8");
+			fos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private Properties createDefaultGuildProperties() {
+		Properties prop = new Properties();
+		prop.put("isCompetitionRunning", "false");
+		prop.put("submitChannel", "0");
+		prop.put("organiserChannel", "0");
+		return prop;
+	}
+	
+	@Override
+	public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+		LOGGER.info("Running slash command {}", event.getCommandPath());
+		event.deferReply().queue(hook -> {
+			Util.sendDeletableMessage(event.getChannel(), "Nothing useful yet");
+			hook.deleteOriginal().queue();
+		});
+	}
+	
+	@Override
+	public void onMessageReactionAdd(MessageReactionAddEvent event) {
+		if (!Util.isThisUserThisBot(event.getUser())) {
+
+			ReactionEmote reactionEmote = event.getReactionEmote();
+
+			if (EmoteWrapper.getReactionEmoteId(reactionEmote).equals(EmojiManager.getForAlias(":x:").getUnicode())) {
+
+				event.retrieveMessage().queue(msg -> {
+					if (Util.isThisUserThisBot(msg.getAuthor())) {
+
+						if (Util.hasBotReactedWith(msg, EmojiManager.getForAlias(":x:").getUnicode())) {
+							Util.deleteMessage(msg);
+						}
+					}
+				});
+			}
+		}
+	}
+	
+	public static TASCompBot getBot() {
+		return instance;
+	}
+
+	public JDA getJDA() {
+		return jda;
 	}
 }
---
+++ src/main/java/com/minecrafttas/tascomp/util/Util.java
@@ -0,0 +1,177 @@
+package com.minecrafttas.tascomp.util;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import com.vdurmont.emoji.EmojiManager;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.MessageBuilder;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.MessageReaction;
+import net.dv8tion.jda.api.entities.MessageReaction.ReactionEmote;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.User;
+
+public class Util {
+	
+	/**
+	 * Sends a message to the channel
+	 * @param channel A message channel
+	 * @param message A string message
+	 */
+	public static void sendMessage(MessageChannel channel, String message) {
+		channel.sendMessage(message).queue();
+	}
+	
+	/**
+	 * Sends a message to the channel
+	 * @param channel A message channel
+	 * @param message A message object
+	 */
+	public static void sendMessage(MessageChannel channel, Message message) {
+		channel.sendMessage(message).queue();
+	}
+	
+	/**
+	 * Adds an X reactions to the message that deletes the message if it's being pressed
+	 * @param channel
+	 * @param message
+	 */
+	public static void sendDeletableMessage(MessageChannel channel, String message) {
+		channel.sendMessage(message).queue(msg-> msg.addReaction(EmojiManager.getForAlias(":x:").getUnicode()).queue());
+	}
+	
+	/**
+	 * Adds an X reactions to the message that deletes the message if it's being pressed
+	 * @param channel
+	 * @param message
+	 */
+	public static void sendDeletableMessage(MessageChannel channel, Message message) {
+		channel.sendMessage(message).queue(msg-> msg.addReaction(EmojiManager.getForAlias(":x:").getUnicode()).queue());
+	}
+	
+	/**
+	 * Sends a message that is being removed after a specified amount of time
+	 * @param channel The channel
+	 * @param message The message
+	 * @param time The time in seconds after it is being deleted
+	 */
+	public static void sendSelfDestructingMessage(MessageChannel channel, String message, int time) {
+		channel.sendMessage(message).queue(msg -> {
+			msg.delete().queueAfter(time, TimeUnit.SECONDS);
+		});
+	}
+	
+	/**
+	 * Sends a message that is being removed after a specified amount of time
+	 * @param channel The channel
+	 * @param message The message
+	 * @param time The time in seconds after it is being deleted
+	 */
+	public static void sendSelfDestructingMessage(MessageChannel channel, Message message, int time) {
+		channel.sendMessage(message).queue(msg -> {
+			msg.delete().queueAfter(time, TimeUnit.SECONDS);
+		});
+	}
+	
+	public static void deleteMessage(Message msg) {
+		msg.delete().queue();
+	}
+	
+	public static void deleteMessage(Message msg, String reason) {
+		msg.delete().reason(reason).queue();
+	}
+	
+	public static boolean hasRole(Member member, String... roleNames) {
+		List<Role> roles=member.getRoles();
+		
+		for(String rolename : roleNames) {
+			for(Role role : roles) {
+				if(role.getName().equalsIgnoreCase(rolename)) {
+					return true;
+				}
+			}
+		}
+	
+		return false;
+	}
+	
+	/**
+	 * @param user The user from an event or an author
+	 * @return If the user is the bot account this is running on
+	 */
+	public static boolean isThisUserThisBot(User user) {
+		return user.getJDA().getSelfUser().getIdLong() == user.getIdLong();
+	}
+	
+	/**
+	 * @param member The member to check
+	 * @return If the member has MESSAGE_MANAGE permissions
+	 */
+	public static boolean hasEditPerms(Member member) {
+		return member.hasPermission(Permission.MESSAGE_MANAGE);
+	}
+	
+	/**
+	 * @param member The member to check
+	 * @return If the member has ADMINISTRATOR permissions
+	 */
+	public static boolean hasAdminPerms(Member member) {
+		return member.hasPermission(Permission.ADMINISTRATOR);
+	}
+	
+	/**
+	 * @param member The member to check
+	 * @return If the member has MANAGE_WEBHOOKS permissions
+	 */
+	public static boolean hasIntegrationPerms(Member member) {
+		return member.hasPermission(Permission.MANAGE_WEBHOOKS);
+	}
+	
+	/**
+	 * Checks if the bot has reacted with an emote
+	 * @param msg The message to check
+	 * @param emote The emote to check
+	 * @return If the bot has reacted with an emote
+	 */
+	public static boolean hasBotReactedWith(Message msg, String emote) {
+		//Iterate through all reactions
+		for(MessageReaction reaction : msg.getReactions()) {
+			
+			ReactionEmote rEmote=reaction.getReactionEmote();
+			if(rEmote.isEmoji()){
+				if(rEmote.getEmoji().equals(emote)) {
+					return reaction.isSelf();
+				}
+			} else if(rEmote.isEmote()) {
+				if(rEmote.getEmote().getName().equals(emote)) {
+					return reaction.isSelf();
+				}
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Sends an error message from an exception
+	 * @param channel
+	 * @param e
+	 */
+	public static void sendErrorMessage(MessageChannel channel, Exception e) {
+		String message="The error has no message .__.";
+		if(e.getMessage()!=null) {
+			message=e.getMessage();
+		}
+		Message msg=new MessageBuilder(new EmbedBuilder().setTitle("Error ._.").addField(e.getClass().getSimpleName(), message, false).setColor(0xB90000)).build();
+		sendDeletableMessage(channel, msg);
+	}
+
+	public static Message constructEmbedMessage(String title, String fieldtext, String text, int color) {
+		return new MessageBuilder(new EmbedBuilder().setTitle(title).addField(fieldtext, text, false).setColor(color)).build();
+	}
+
+}
